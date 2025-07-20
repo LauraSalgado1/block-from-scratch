fc7155e5407e9cf5cad95fe4a64236e170e4f959
Laura Salgado <culham.laura@gmail.com>
2025-07-19 21:31:55 -0400

    add copyright date block

---
+++ wp-content/plugins/copyright-date-block/build/copyright-date-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a149fb5d4fc426e1aea6');
--- wp-content/plugins/laura-salgado-questionnaire/build/index.asset.php
+++ wp-content/plugins/laura-salgado-questionnaire/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => 'e03b49d7fd8dd0e57b51');
+<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => 'a8dd8c3701cbb7cfaa42');
